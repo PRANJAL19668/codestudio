ac7a362df323f78021f8c74d108a397bcd5aa985
PRANJAL19668 <pranjalyadav250@gmail.com>
2023-09-01 06:38:18 +0530

    add layout into views

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const app=express();
 const path=require('path');
 //setting up the express-layouts
 const expressLayouts=require('express-ejs-layouts');
-//using expressLayouts
-app.use('expressLayouts');
-
-//use express router
-app.use('/', require('./routes'));
 //setting up the view engine
 app.set('view engine','ejs');
 //setting up the path
@@ -30,6 +25,8 @@ app.use('expressLayouts');
 
 
 
+//use express router
+app.use('/', require('./routes'));
 
 //server
 app.listen(port,function(err){
