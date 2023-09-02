2d32c9897ba0b123af4db05715efc667a17b50b5
PRANJAL19668 <pranjalyadav250@gmail.com>
2023-09-02 20:41:23 +0530

    add changes in the users_controller.js,users.js,user_sign_in.ejs,user_sign_up.ejs

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -52,7 +52,7 @@ module.exports.signin=function(req,res){
 //   }
 // };
 module.exports.create= async function(req,res){
-  if(req.body.password != req.body.confirm_password){
+  if(req.body.password != req.body.Confirm_password){
     return res.redirect('back');
   }
   try{
@@ -74,28 +74,51 @@ module.exports.create= async function(req,res){
 
 //CREATING SIGN-IN SESSION
 //USING ASYNC AWAIT ONLY WITH FUNCTIONS WHEN USE.
-module.exports.createSession = async function(req,res){
-  //find the user
-  try{
-    const user = await User.findOne ({email: req.body.email});
+// module.exports.createSession = async function(req,res){
+//   //find the user
+//   try{
+//     const user = await User.findOne({email: req.body.email});
 
-  } catch(err){
-    console.log('Error in creating user while signing-in',err);
-  }
-  if(User){
-    //user password which does not matches.
-    if(User.password != req.body.password){
+//   } catch(err){
+//     console.log('Error in creating user while signing-in',err);
+//   }
+//   if(User){
+//     //user password which does not matches.
+//     if(User.password != req.body.password){
+//       return res.redirect('back');
+//     }
+//     //handle user creation
+//     //if the password matches,we set the cookie
+//     res.cookie('user_id',User._id)
+//       return res.redirect('/users/profile');
+//     }
+//     else{
+//       return res.redirect('back');
+//     }
+
+//   }
+
+module.exports.createSession = async function(req, res) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      // Check if the provided password matches the user's password
+      if (user.password === req.body.password) {
+        // If the password matches, set the user_id cookie
+        res.cookie('user_id', user._id);
+        return res.redirect('/users/profile');
+      } else {
+        // Password does not match, redirect back
+        return res.redirect('back');
+      }
+    } else {
+      // User with the given email doesn't exist, redirect back
       return res.redirect('back');
     }
-    //handle user creation
-    //if the password matches,we set the cookie
-    res.cookie('user_id',User._id)
-      return res.redirect('/users/profile');
-    }
-    else{
-      return res.redirect('back');
-    }
-
-  }
-
-
+  } catch (err) {
+    console.log('Error in creating user while signing-in', err);
+    // Handle the error appropriately, e.g., send an error response
+    return res.status(500).send('Internal Server Error');
+};
+}
--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.get('/profile',usersController.profile);
 router.get('/sign-up',usersController.signup);
 router.get('/sign-in',usersController.signin);
 router.post('/create',usersController.create);
-router.post('/create-session',usersController.createSession);
+router.post('/create-Session',usersController.createSession);
 
 
 
