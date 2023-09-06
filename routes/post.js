c08ab427b8c251b29e3f0d01b0955bd4efa0a021
PRANJAL19668 <pranjalyadav250@gmail.com>
2023-09-06 12:17:18 +0530

    add changes in the post.js/routes

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router= express.Router();
+const passport = require('passport');
 //defined postController
 const postController = require('../controller/posts_controller');
 //access the postController in rotes
-router.post('/create',postController.create);
+router.post('/create',passport.checkAuthentication,postController.create);
 
 
 
