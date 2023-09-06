972f4136e2c4988cd7bf30cb32f2d20ef3472f3d
PRANJAL19668 <pranjalyadav250@gmail.com>
2023-09-06 11:21:48 +0530

    add changes in the home_controller.js,posts_controller.js,home.ejs

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,11 +1,23 @@
+const Post = require('../models/post');
 //module.exports.actionName=function(req,res){}
-module.exports.home=function(req,res){
+module.exports.home= async function(req,res){
     //checking cookies if exist or not.
-    console.log(req.cookies);
+    //console.log(req.cookies);
     //changing the value of cookies
-    res.cookie('user_id',87);
+   // res.cookie('user_id',87);
     //return res.end('<h1>Express  is up for the codestudio!</h1>');
-    return res.render('home',{
-        title:'Home'
-    });
+    try{
+        let postsdata = await Post.find({});
+        return res.render('home',{
+            title:'HOME',
+            //json object created
+            //1st post is the property which we are giving in home.ejs i.e.. (post of posts)
+            //2nd postdata  is the value which we are giving in the line no 10 ie..postdata
+            posts : postsdata
+        });
+
+    } catch (err){
+        console.log('Error in finding the post',err);
+        return res.redirect('back');
+    }
 }
--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -9,6 +9,7 @@ module.exports.create = async function(req,res){
             content: req.body.content,
             user: req.user._id
         });
+        return res.redirect('back');
     } catch (err){
         console.log('Error in creating a post',err);
         return res.redirect('back');
