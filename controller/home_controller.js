6402a360b899ef04967f7b1a721b3e790b866032
PRANJAL19668 <pranjalyadav250@gmail.com>
2023-09-08 10:54:52 +0530

    add changes in home_controller.js,home.ejs

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,6 +1,7 @@
-const Post = require('../models/post');
+//.. means one level up because we are in controllers.
+const Post =  require('../models/post');
 //module.exports.actionName=function(req,res){}
-module.exports.home= async function(req,res){
+module.exports.home = async function(req,res){
     //checking cookies if exist or not.
     //console.log(req.cookies);
     //changing the value of cookies
@@ -8,6 +9,7 @@ module.exports.home= async function(req,res){
    // res.cookie('user_id',87);
     //return res.end('<h1>Express  is up for the codestudio!</h1>');
     try{
+        // finding the post with Post.find({})
         let postsdata = await Post.find({}).
         //populating the user of each post ie.. showing the name of each post.
         //.exec() is the callback function.
@@ -15,8 +17,8 @@ module.exports.home= async function(req,res){
         return res.render('home',{
             title:'HOME',
             //json object created
-            //1st post is the property which we are giving in home.ejs i.e.. (post of posts)
-            //2nd postdata  is the value which we are giving in the line no 10 ie..postdata
+            //1st posts is the property which we are giving in home.ejs i.e.. (post of posts)
+            //2nd postsdata  is the value which we are giving in the line no 13 ie..postsdata
             posts : postsdata
         });
 
