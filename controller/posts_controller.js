4ee6f184d5eb3bf5ffbb7d7ca561f98454b8c8ac
PRANJAL19668 <pranjalyadav250@gmail.com>
2023-09-09 20:36:15 +0530

    add changes in the post_controller.js,post.js,home.ejs

--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -3,6 +3,7 @@
 //.. one level up
 //using async await with functions 
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 module.exports.create = async function(req,res){
     try{
         let post = await Post.create({
@@ -12,6 +13,39 @@ module.exports.create = async function(req,res){
         return res.redirect('back');
     } catch (err){
         console.log('Error in creating a post',err);
+        return res.redirect('back');
+    }
+}
+
+
+
+
+
+//DELETING A POST
+//USING ASYNC AWAIT WITH FUNCTIONS.
+
+module.exports.destroy = async function(req,res){
+    try{
+        let post = await Post.findById(req.params.id); //string params
+        //AUTHORIZATION,IS I AM ALLOWED TO DELETE A POST THAT HAS BEEN WRIITEN BY MY FRIEND OR VICE VERSA.
+        //CHECKING THE USER IF IT IS THE SAME USER WHO HAS CREATED THE POST, IS THE SAME USER WHO IS DELETING THE POST.
+        //INITIALLY,ITS IS THE ID OF THE USER( RETURN STRING ID),UNTIL UNLESS I CHOOSE TO POPULATE THE USER(PRELOADED).
+        //.id means convert the objectId into string
+        if(post.user == req.user.id){
+            //if both are same,simply remove the post i.e..current logged user.
+            post.remove();
+            //using promises
+            //commment.deleteMany({}) is the function which  deletes all the comments based on query passed.
+            await Comment.deleteMany({post: req.params.id}).then(function(post){
+                return res.redirect('back');
+            });
+        } else{
+            //if the user is not same,then simply return back.
+            return res.redirect('back');
+        }
+
+    } catch(err){
+        console.log('Error in fetching/deleting a post',err);
         return res.redirect('back');
     }
 }
--- routes/post.js
+++ routes/post.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const postController = require('../controller/posts_controller');
 //access the postController in routes
 router.post('/create', passport.checkAuthentication, postController.create);
-
+router.get('/destroy/:id', passport.checkAuthentication , postController.destroy);
 
 
 module.exports = router;
